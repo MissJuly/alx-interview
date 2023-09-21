4e4b96f488b614944ea7bc45192041a7c2f386ee
MissJuly <joygitau98@gmail.com>
2023-09-21 23:02:10 +0300

    fix errors

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,7 +11,7 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # create an array to store the min number of coins
+    # create an array to store the min number of coins ---
     min_coins = [float('inf')] * (total + 1)
     min_coins[0] = 0
 
@@ -21,4 +21,4 @@ def makeChange(coins, total):
             min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
 
         # return the min number of coins required to reach the total value
-    return min_coins[total] if min_coins[total] !=  float('inf') else -1
+    return min_coins[total] if min_coins[total] != float('inf') else -1
